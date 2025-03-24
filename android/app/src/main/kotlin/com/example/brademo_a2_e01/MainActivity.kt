67fbcf3b887a9b037f2569c8d2d4433380f11ef3
anaangelieri <ana.angelieri@aluno.ifsp.edu.br>
2025-03-24 15:46:45 -0300

    Primeiro commit

---
+++ android/app/src/main/kotlin/com/example/brademo_a2_e01/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.brademo_a2_e01
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+// This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter is Fun!'),
+          backgroundColor: Colors.green,
+        ),
+        body: Align(
+          alignment: Alignment(
+              -0.5, -0.6), // Define a posição no canto superior esquerdo
+          child: Container(
+            width: 100,
+            height: 100,
+            padding: EdgeInsets.all(8.0),
+            color: Colors.red,
+            child: Text(
+              'Hi Mom 🐣',
+              style: TextStyle(color: Colors.black),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
